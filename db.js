685b66ff48ca6395e6d65d1e0b29da6041249aa3
Daniel Chen <team6612@gmail.com>
2017-02-20 20:48:50 +0800

    first commit

---
+++ db.js
@@ -0,0 +1,93 @@
+var config = require('config');
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+var f = require('util').format;
+var url = config.get('mongo.url');
+var user = config.get('mongo.user');
+var pwd = config.get('mongo.pwd');
+
+var url = f('mongodb://%s:%s@%s', user, pwd, url);
+
+function checkFlag(flag, callback) {
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var collection = db.collection('flags');
+    collection.find({"flag": flag}).toArray(function(err, docs) {
+      assert.equal(err, null);
+      return callback(docs);
+    });
+  })
+}
+
+function getUser(name, callback) {
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var collection = db.collection('scores');
+    collection.find({"name": name}).toArray(function(err, docs) {
+      assert.equal(err, null);
+      return callback(docs);
+    });
+  })
+}
+
+function updateUser(user, flag, callback) {
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var fcollection = db.collection('flags');
+    fcollection.updateOne({"flag": flag.flag}, {
+      $set: { solved: flag.solved+1 }
+    },function() {
+      var collection = db.collection('scores');
+      collection.updateOne({"name": user.name}, {
+        $set: {
+          solved: user.solved,
+          score: user.score
+        }
+      }, callback);
+    });
+  })
+}
+
+function addUser(name, callback) {
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var collection = db.collection('scores');
+    collection.insert({name: name, score: 0, solved:[]}, callback);
+  })
+}
+
+function getRank(callback) {
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var collection = db.collection('scores');
+    collection.find({}).sort({score: -1}).toArray(function(err, docs) {
+      assert.equal(err, null);
+      return callback(docs);
+    });
+  })
+}
+
+function getProblems(callback) {
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var collection = db.collection('flags');
+    collection.find({}).sort({type:1, no:1}).toArray(function(err, docs) {
+      assert.equal(err, null);
+      docs.map(function(d) {
+        delete d.flag;
+        return d;
+      })
+      return callback(docs);
+    });
+  })
+}
+
+module.exports = {
+  checkFlag: checkFlag,
+  getUser: getUser,
+  updateUser: updateUser,
+  addUser: addUser,
+  getRank: getRank,
+  getProblems: getProblems
+}
---
+++ server.js
@@ -0,0 +1,128 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var db = require('./db');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/api/submit', function(req, res) {
+  var id = req.query.id;
+  var flag = req.query.flag;
+  var resp = {};
+  resp.status = -1;
+  resp.msg = 'Not yet send query.';
+  if (typeof id !== 'undefined' && id !== '') {
+    db.checkFlag(flag, function(f) {
+      if(f.length > 0) {
+        f = f[0];
+        db.getUser(id, function(u) {
+          if(u.length == 1) {
+            u = u[0];
+            if(u.solved.indexOf(f.type+"-"+f.no)!=-1) {
+              // Have been solved.
+              resp.status = 3;
+              resp.msg = 'Duplicate submit.';
+              res.jsonp(resp);
+            } else {
+              // Solve! push solved and add score
+              u.solved.push(f.type+"-"+f.no);
+              u.score += f.score;
+              db.updateUser(u, f, function(){
+                resp.status = 0;
+                resp.msg = 'Success!';
+                res.jsonp(resp);
+              });
+            }
+          } else if(u.length == 0) {
+            // user not found.
+            db.addUser(id, function() {
+              db.getUser(id, function(u) {
+                u = u[0];
+                if(u.solved.indexOf(f.type+"-"+f.no)!=-1) {
+                  // Have been solved.
+                  resp.status = 3;
+                  resp.msg = 'Duplicate submit.';
+                  res.jsonp(resp);
+                } else {
+                  // Solve! push solved and add score
+                  u.solved.push(f.type+"-"+f.no);
+                  u.score += f.score;
+                  db.updateUser(u, f, function(){
+                    resp.status = 0;
+                    resp.msg = 'Success!';
+                    res.jsonp(resp);
+                  });
+                }
+              });
+            })
+          }
+        })
+      } else {
+        // flag not found.
+        resp.status = 1;
+        resp.msg = 'Flag not found.';
+        res.jsonp(resp);
+      }
+    })
+  } else {
+    resp.status = 1;
+    resp.msg = 'ID not set!';
+    res.jsonp(resp);
+  }
+})
+
+app.get('/api/new', function(req, res) {
+  var id = req.query.id;
+  var resp = {};
+  resp.status = -1;
+  resp.msg = 'Not yet send query.';
+  if (typeof id !== 'undefined' && id !== '') {
+    db.getUser(id, function(docs) {
+      if(!docs.length) {
+        db.addUser(id, function() {
+          resp.status = 0;
+          resp.msg = 'Success!';
+          res.jsonp(resp);
+        })
+      } else {
+        resp.status = 2;
+        resp.msg = 'User duplicated!';
+        res.jsonp(resp);
+      }
+    })
+  } else {
+    resp.status = 1;
+    resp.msg = 'ID not set!';
+    res.jsonp(resp);
+  }
+})
+
+app.get('/api/rank', function(req, res) {
+  var id = req.query.id;
+  var resp = {};
+  resp.status = -1;
+  resp.msg = 'Not yet send query.';
+  db.getRank(function(r) {
+    resp.status = 0;
+    resp.msg = 'Success!';
+    resp.data = r;
+    res.jsonp(resp);
+  })
+})
+
+app.get('/api/problems', function(req, res) {
+  var id = req.query.id;
+  var resp = {};
+  resp.status = -1;
+  resp.msg = 'Not yet send query.';
+  db.getProblems(function(r) {
+    resp.status = 0;
+    resp.msg = 'Success!';
+    resp.data = r;
+    res.jsonp(resp);
+  })
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+})
